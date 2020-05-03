5052a8f4ada4f8a12fd9fa5aba50a048d3d02d74
amit12soni <51396574+amit12soni@users.noreply.github.com>
2020-05-03 16:19:45 +0530

    Add files via upload

---
+++ history.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Mar 26 20:11:02 2020)---
+import CV2
+import cv2
+
+## ---(Fri Mar 27 11:07:13 2020)---
+import tensorflow as tf
+
+## ---(Sat Mar 28 11:52:42 2020)---
+import numpy as np
+clr
+cls
+
+## ---(Sun Mar 29 16:07:14 2020)---
+import cv2
+cls
+runfile('C:/Users/ab/.spyder-py3/Camera.py', wdir='C:/Users/ab/.spyder-py3')
+q
+cls
+runfile('C:/Users/ab/.spyder-py3/Camera.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/Camera.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/Camera.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/Camera.py', wdir='C:/Users/ab/.spyder-py3')
+
+## ---(Sun Mar 29 18:16:57 2020)---
+runfile('C:/Users/ab/.spyder-py3/Camera.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+
+## ---(Sun Mar 29 18:18:34 2020)---
+runfile('C:/Users/ab/.spyder-py3/Rescal.py', wdir='C:/Users/ab/.spyder-py3')
+
+## ---(Mon Mar 30 12:09:13 2020)---
+runfile('C:/Users/ab/.spyder-py3/Recordavideo.py', wdir='C:/Users/ab/.spyder-py3')
+
+## ---(Tue Mar 31 10:56:18 2020)---
+runfile('C:/Users/ab/.spyder-py3/faceidentification.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/faceidentification.py', wdir='C:/Users/ab/.spyder-py3')
+
+## ---(Tue Mar 31 19:57:39 2020)---
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+
+## ---(Wed Apr  1 15:03:44 2020)---
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+runfile('C:/Users/ab/.spyder-py3/faceidentification.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+runfile('C:/Users/ab/.spyder-py3/faceidentification.py', wdir='C:/Users/ab/.spyder-py3')
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+
+## ---(Wed Apr  1 16:51:09 2020)---
+runfile('C:/Users/ab/.spyder-py3/Trainface.py', wdir='C:/Users/ab/.spyder-py3')
+runfile('C:/Users/ab/.spyder-py3/faceidentification.py', wdir='C:/Users/ab/.spyder-py3')
+cls
+
+## ---(Wed Apr  1 17:49:13 2020)---
+runfile('C:/Users/ab/.spyder-py3/faceidentification.py', wdir='C:/Users/ab/.spyder-py3')
+
+## ---(Sat May  2 13:29:15 2020)---
+runfile('C:/Users/ab/Reinforcement Learning.py', wdir='C:/Users/ab')
